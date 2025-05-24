4c5da9c80b77e917c790f7d89cb9794d34f4928f
Abdulaziz Hamidjonov <163741823+abdulaziz-python@users.noreply.github.com>
2025-05-24 22:32:47 +0500

    Update throttling.py

--- bot/middlewares/throttling.py
+++ bot/middlewares/throttling.py
@@ -1,33 +1,35 @@
-from typing import Any, Awaitable, Callable, Dict
-import time
 from aiogram import BaseMiddleware
-from aiogram.types import Message, TelegramObject
+from aiogram.types import TelegramObject, Message, CallbackQuery
 from bot.utils.logger import get_logger
+import time
+from collections import defaultdict
 
 logger = get_logger(__name__)
 
 class ThrottlingMiddleware(BaseMiddleware):
-    def __init__(self, limit=0.5):
-        self.limit = limit
-        self.user_timeouts = {}
+    def __init__(self):
+        self.requests = defaultdict(list)
+        self.rate_limit = 1
+        self.time_window = 1
 
-    async def __call__(
-        self,
-        handler: Callable[[TelegramObject, Dict[str, Any]], Awaitable[Any]],
-        event: TelegramObject,
-        data: Dict[str, Any]
-    ) -> Any:
-        if not isinstance(event, Message):
+    async def __call__(self, handler, event: TelegramObject, data: dict):
+        user_id = event.from_user.id if isinstance(event, (Message, CallbackQuery)) else None
+        if not user_id:
             return await handler(event, data)
-
-        user_id = event.from_user.id
         current_time = time.time()
-
-        if user_id in self.user_timeouts:
-            last_time = self.user_timeouts[user_id]
-            if current_time - last_time < self.limit:
-                logger.info(f"Throttling applied for user {user_id}")
-                return None
-
-        self.user_timeouts[user_id] = current_time
+        self.requests[user_id] = [
+            t for t in self.requests user_id] if t > current_time - self.time_window
+        ]
+        if len(self.requests[user_id]) >= self.rate_limit:
+            if isinstance(event, Message):
+                await event.answer(
+                    "⏳ Iltimos, biroz kuting va qayta urinib ko'ring."
+                )
+            elif isinstance(event, CallbackQuery):
+                await event.answer(
+                    "⏳ Iltimos, biroz kuting va qayta urinib ko'ring.",
+                    show_alert=True
+                )
+            return
+        self.requests[user_id].append(current_time)
         return await handler(event, data)
