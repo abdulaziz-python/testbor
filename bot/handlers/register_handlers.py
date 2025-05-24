c3cde45f842681d1fa7900a8feb83f2411ff56d5
Abdulaziz Hamidjonov <163741823+abdulaziz-python@users.noreply.github.com>
2025-05-24 22:29:48 +0500

    update: added new handlers

--- bot/handlers/register_handlers.py
+++ bot/handlers/register_handlers.py
@@ -1,6 +1,16 @@
 from aiogram import Dispatcher
-from bot.handlers import user, admin
+from bot.handlers.user import router as user_router
+from bot.handlers.admin import router as admin_router
+from bot.middlewares.error_handler import ErrorHandlerMiddleware
+from bot.middlewares.subscription import SubscriptionMiddleware
+from bot.middlewares.throttling import ThrottlingMiddleware
 
 def register_handlers(dp: Dispatcher):
-    dp.include_router(user.router)
-    dp.include_router(admin.router)
+    dp.message.middleware(ErrorHandlerMiddleware())
+    dp.callback_query.middleware(ErrorHandlerMiddleware())
+    dp.message.middleware(SubscriptionMiddleware())
+    dp.callback_query.middleware(SubscriptionMiddleware())
+    dp.message.middleware(ThrottlingMiddleware())
+    dp.callback_query.middleware(ThrottlingMiddleware())
+    dp.include_router(user_router)
+    dp.include_router(admin_router)
